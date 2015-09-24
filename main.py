33bbda3acca4859c36ab9cf35609f5b0163c00cd
WindowsHyun <kwon616@gmail.com>
2015-09-25 01:30:11 +0900

    TermProject 20150925 Start!
    PyCharm 프로젝트 제작 및 작업 시작.

---
+++ main.py
@@ -0,0 +1,10 @@
+__author__ = 'WindowsHyun'
+
+import os
+os.chdir('c:\\2DGraphics\\2DGraphics')
+print("import OS And Dir Setting")
+
+from pico2d import *
+open_canvas()
+print("import Pico2D And Open Canvas")
+
