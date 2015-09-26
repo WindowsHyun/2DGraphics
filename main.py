17ba22650c225c79cff0cbcf679f7075ee140f9d
WindowsHyun <kwon616@gmail.com>
2015-09-27 00:12:36 +0900

    Update main.py
    pico2d 메인화면이 뜨자마자 바로 꺼지는 부분 해결 아직 종료하는 방법을 모름...
    isRunning 을 False로 만들면 될듯하나 어디서 False를 해줘야 하는지...

--- main.py
+++ main.py
@@ -5,7 +5,27 @@ os.chdir('c:\\2DGraphics\\2DGraphics')
 print("import OS And Dir Setting")
 
 from pico2d import *
-open_canvas()
+isCanvasWidth = 800
+isCanvasHeight = 600
+isRunning = True
 print("import Pico2D And Open Canvas")
 
-delay(3)
+def handle_events():
+    global isRunning
+
+    events = get_events()
+
+def main():
+    open_canvas(isCanvasWidth, isCanvasHeight)
+
+    while isRunning:
+        handle_events()
+        clear_canvas()
+        update_canvas()
+        #running = False
+        delay(0.08)
+
+    close_canvas()
+
+if __name__ == '__main__':
+    main()
