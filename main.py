7c25cacbdddfbb5e8e01bdb344dbfeb8ef1b1be5
WindowsHyun <kwon616@gmail.com>
2015-10-01 19:21:09 +0900

    Update main.py
    480*800 크기 조절 & 종료버튼 및 esc 버튼 클릭시 종료하는 방법 추가!

--- main.py
+++ main.py
@@ -5,27 +5,35 @@ os.chdir('c:\\2DGraphics\\2DGraphics')
 print("import OS And Dir Setting")
 
 from pico2d import *
-isCanvasWidth = 800
-isCanvasHeight = 600
-isRunning = True
-print("import Pico2D And Open Canvas")
+print("import Pico2D")
+global gCanvasWidth, gCanvasHeight
+gCanvasWidth = 480
+gCanvasHeight = 800
+gRunning = True
+print("Create Local -> Global function")
 
 def handle_events():
-    global isRunning
-
+    global gRunning
     events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT or event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            # 종료버튼을 누르거나 or 키보드의 ESC 키를 누를경우 종료를 한다.
+            gRunning = False
+            print("Bye Bye~!!!")
+
+    pass
 
 def main():
-    open_canvas(isCanvasWidth, isCanvasHeight)
+    open_canvas(gCanvasWidth, gCanvasHeight)
 
-    while isRunning:
-        handle_events()
+    while (gRunning):
         clear_canvas()
         update_canvas()
-        #running = False
-        delay(0.08)
+        delay(0.05)
+        handle_events()
 
     close_canvas()
+
 
 if __name__ == '__main__':
     main()
