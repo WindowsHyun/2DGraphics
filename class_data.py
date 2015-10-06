dd6a9cc3594c7eddcf2bbfc705396966c9cc9ab9
WindowsHyun <kwon616@gmail.com>
2015-10-06 23:23:09 +0900

    Update py Code
    클래스 함수 코드 사용 최소화 및, 다음 차수에 구현할 부분에 대하여 테스트 함.
    필요 없는 클래스 안의 함수 제거

--- class_data.py
+++ class_data.py
@@ -8,16 +8,20 @@ class cBackGround:
     def __init__(self):
         self.image = load_image('BackgroundImage\\SBT.png')
         #print("Import Backgroung Data")
-    def draw(self):
-        self.image.draw_to_origin(0, main.gY, main.gCanvasWidth, main.gCanvasHeight)
-        # draw_to_origin을 사용하면 원본이미지를 그대로 사용 가능하면서 사이즈 크기를 자신이 원하는만큼 조절이 가능하다.
-    def drawTwo(self):
-        self.image.draw_to_origin(0, main.gY2, main.gCanvasWidth, main.gCanvasHeight)
-        #drawTwo를 만든이유는 이미지가 내려오는데 중간에 끊겨보이면 안되니깐 자연스럽게 이어지게 만들기 위하여.
-    def dAimage(self):
-        self.image = load_image('BackgroundImage\\DBT.png')
-    def dBimage(self):
-        self.image = load_image('BackgroundImage\\BSBT.png')
+    def draw(self, gWhatDraw):
+        if gWhatDraw == 0:
+            self.image.draw_to_origin(0, main.gY, main.gCanvasWidth, main.gCanvasHeight)
+            # draw_to_origin을 사용하면 원본이미지를 그대로 사용 가능하면서 사이즈 크기를 자신이 원하는만큼 조절이 가능하다.
+        else:
+            self.image.draw_to_origin(0, main.gY2, main.gCanvasWidth, main.gCanvasHeight)
+            #drawTwo를 만든이유는 이미지가 내려오는데 중간에 끊겨보이면 안되니깐 자연스럽게 이어지게 만들기 위하여.
+    def dChangeBackground(self, gNum):
+        if gNum == 0:
+            self.image = load_image('BackgroundImage\\SBT.png')
+        if gNum == 1:
+            self.image = load_image('BackgroundImage\\BSBT.png')
+        if gNum == 2:
+            self.image = load_image('BackgroundImage\\DBT.png')
     pass
 
 class cDrawPlanet:
@@ -25,6 +29,18 @@ class cDrawPlanet:
         self.image = load_image('GeneralImage\\planet.png')
         self.image.draw(415, 723)
     pass
+
+"""
+class cDrawRabbit:
+    def dDraw(self, frame, LR):
+        if LR == True:
+            self.image = load_image('CharacterImage\\Rabbit-Left.png')
+        elif LR == False:
+            self.image = load_image('CharacterImage\\Rabbit-Right.png')
+
+        self.image.clip_draw(frame * 85, 0, 85, 113, 100, 100)
+    pass
+"""
 
 class cAutoBackGround:
     def __init__(self):
--- main.py
+++ main.py
@@ -15,30 +15,36 @@ gCanvasWidth = 480
 gCanvasHeight = 800
 gY = 0
 gY2 = 800
+gFrame = 0
 gRunning = True
+gLeftTRightF = True
 gBackGround = 0
 print("Create Local -> Global function")
 
 def handle_events():
-    global gRunning
-    global gBackGround
+    global gRunning, gBackGround, gFrame, gLeftTRightF
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT or event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             # 종료버튼을 누르거나 or 키보드의 ESC 키를 누를경우 종료를 한다.
             gRunning = False
             print("Bye Bye~!!!")
+        if event.type == SDL_KEYDOWN and event.key == SDLK_KP_0:
+            gBackGround = 0
         if event.type == SDL_KEYDOWN and event.key == SDLK_KP_1:
             gBackGround = 1
         if event.type == SDL_KEYDOWN and event.key == SDLK_KP_2:
             gBackGround = 2
-        if event.type == SDL_KEYDOWN and event.key == SDLK_KP_3:
-            gBackGround = 3
-
+        if event.type == SDL_KEYDOWN and event.key == SDLK_KP_4:
+            gFrame = (gFrame + 1) % 3
+        if event.type == SDL_KEYDOWN and event.key == SDLK_LEFT:
+            gLeftTRightF = True
+        if event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
+            gLeftTRightF = False
     pass
 
 def main():
-    global gY, gY2
+    global gY, gY2, gFrame, gLeftTRightF
     open_canvas(gCanvasWidth, gCanvasHeight)
     lBackGround = class_data.cBackGround()
     # cBackGround라는 클래스를 BackGround로 가져오기
@@ -47,28 +53,33 @@ def main():
     lPlanet = class_data.cDrawPlanet()
     # 클래스 함수를 만들어서 행성이 보이게 만들기
 
+    """
+    lRabbit = class_data.cDrawRabbit()
+    """
+
     while (gRunning):
         clear_canvas()
 
         lAuto.__init__()
 
-        if gBackGround == 1:
-            lBackGround.dAimage()
-        if gBackGround == 2:
-            lBackGround.__init__()
-        if gBackGround == 3:
-            lBackGround.dBimage()
+
+        lBackGround.dChangeBackground(gBackGround)
+
         # 이미지 로드를 바꾸면 클래스 내에서도 여러 이미지를 불러올수있다!
 
-        lBackGround.draw()
-        lBackGround.drawTwo()
+        lBackGround.draw(0)
+        lBackGround.draw(1)
         # 해당 클래스에서 이미지 그려주기.
         lPlanet.__init__()
         # 배경 그려주고 난뒤 행성 그려주기.
 
+        """
+        lRabbit.dDraw(gFrame, gLeftTRightF)
+        """
+
         update_canvas()
-        delay(0.05)
         handle_events()
+        delay(0.015)
 
     close_canvas()
 
