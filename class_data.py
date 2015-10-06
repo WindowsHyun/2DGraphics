e0747c5643bc2a1dce1eb6f878d249eebeb17f06
WindowsHyun <kwon616@gmail.com>
2015-10-06 21:13:20 +0900

    Update class_data, main
    메인코드가 너무 길어지는 이유로 파일 분할
    이미지가 자연스럽게 이어지면서 내려오는 부분 구현
    넘키패드 숫자에 따라서 이미지가 바뀌는 부분 구현

---
+++ class_data.py
@@ -0,0 +1,32 @@
+__author__ = 'Administrator'
+
+from pico2d import *
+
+import main
+
+class cBackGround:
+    def __init__(self):
+        self.image = load_image('BackgroundImage\\SBT.png')
+        #print("Import Backgroung Data")
+    def draw(self):
+        self.image.draw_to_origin(0, main.gY, main.gCanvasWidth, main.gCanvasHeight)
+        # draw_to_origin을 사용하면 원본이미지를 그대로 사용 가능하면서 사이즈 크기를 자신이 원하는만큼 조절이 가능하다.
+    def drawTwo(self):
+        self.image.draw_to_origin(0, main.gY2, main.gCanvasWidth, main.gCanvasHeight)
+        #drawTwo를 만든이유는 이미지가 내려오는데 중간에 끊겨보이면 안되니깐 자연스럽게 이어지게 만들기 위하여.
+    def dAimage(self):
+        self.image = load_image('BackgroundImage\\DBT.png')
+    def dBimage(self):
+        self.image = load_image('BackgroundImage\\BSBT.png')
+    pass
+
+class cAutoBackGround:
+    def __init__(self):
+        global gY, gY2
+        main.gY -= 3
+        main.gY2 -= 3
+
+        if main.gY2 <= 0:
+            main.gY = 0
+            main.gY2 = 800
+    pass
--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 __author__ = 'WindowsHyun'
 
+import class_data
+print("import Class Data py")
+
 import os
+os.chdir('C:\\2DGraphics\\2DGraphics\\ResourceData')
 print("import OS And Dir Settings")
-os.chdir('C:\\2DGraphics\\2DGraphics\\ResourceData\\BackgroundImage')
-
-
-class cBackGround:
-    def __init__(self):
-        self.image = load_image('SBT.png')
-        print("Import Backgroung Data")
-    def draw(self):
-        self.image.draw_to_origin(0, 0, gCanvasWidth, gCanvasHeight)
-        # draw_to_origin을 사용하면 원본이미지를 그대로 사용 가능하면서 사이즈 크기를 자신이 원하는만큼 조절이 가능하다.
-    def dAimage(self):
-        self.image = load_image('DBT.png')
-        print("Import Backgroung Data")
 
 from pico2d import *
 print("import Pico2D")
+
 global gCanvasWidth, gCanvasHeight
 gCanvasWidth = 480
 gCanvasHeight = 800
+gY = 0
+gY2 = 800
 gRunning = True
 gBackGround = 0
 print("Create Local -> Global function")
@@ -36,32 +30,42 @@ def handle_events():
             print("Bye Bye~!!!")
         if event.type == SDL_KEYDOWN and event.key == SDLK_KP_1:
             gBackGround = 1
-            print("",SDLK_KP_1)
         if event.type == SDL_KEYDOWN and event.key == SDLK_KP_2:
             gBackGround = 2
-            print("",SDLK_KP_2)
+        if event.type == SDL_KEYDOWN and event.key == SDLK_KP_3:
+            gBackGround = 3
 
     pass
 
 def main():
+    global gY, gY2
     open_canvas(gCanvasWidth, gCanvasHeight)
-    lBackGround = cBackGround()
-    # cBackGround라는 클래스를 lBackGround로 가져오기
+    lBackGround = class_data.cBackGround()
+    # cBackGround라는 클래스를 BackGround로 가져오기
+    lAuto = class_data.cAutoBackGround()
+    # 클래스 함수를 만들어서 배경화면 내려오게 만들기
+
     while (gRunning):
         clear_canvas()
+
+        lAuto.__init__()
+
         if gBackGround == 1:
             lBackGround.dAimage()
         if gBackGround == 2:
             lBackGround.__init__()
+        if gBackGround == 3:
+            lBackGround.dBimage()
         # 이미지 로드를 바꾸면 클래스 내에서도 여러 이미지를 불러올수있다!
+
         lBackGround.draw()
+        lBackGround.drawTwo()
         # 해당 클래스에서 이미지 그려주기.
         update_canvas()
         delay(0.05)
         handle_events()
 
     close_canvas()
-
 
 if __name__ == '__main__':
     main()
