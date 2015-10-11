787c62d92bd9e0b496baa044816f4c4ee986a5a6
WindowsHyun <kwon616@gmail.com>
2015-10-11 13:39:49 +0900

    Upload gmae_select
    게임 선택화면 추가. 미완성

---
+++ game_select.py
@@ -0,0 +1,108 @@
+__author__ = 'WindowsHyun'
+
+from class_data import *
+print("- import Class Data -")
+import game_framework
+print("- Module game_framework -")
+
+gCanvasWidth = 480
+gCanvasHeight = 800
+gY = 0
+gY2 = 800
+gFrame = 0
+gType = 0
+gRunning = True
+gLeftTRightF = True
+gWhatScenes = "Main"
+# Main, Score, GameSelect, Game 이렇게 4개의 장면을 만들예정.
+# gameframework 작업으로 해당 WhatScenes 는 사라질 예정...
+gBackGround = 0
+print("Create Local -> Global function")
+
+def handle_events():
+    global gRunning, gBackGround, gFrame, gLeftTRightF, gWhatScenes, gType
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT or event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            # 종료버튼을 누르거나 or 키보드의 ESC 키를 누를경우 종료를 한다.
+            game_framework.quit()
+        """
+        if event.type == SDL_KEYDOWN and event.key == SDLK_KP_0:
+            gBackGround = 0
+        if event.type == SDL_KEYDOWN and event.key == SDLK_KP_1:
+            gBackGround = 1
+        if event.type == SDL_KEYDOWN and event.key == SDLK_KP_2:
+            gBackGround = 2
+        """
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            x, y = event.x, gCanvasHeight - event.y
+            gWhatScenes = dMenuClick(gWhatScenes, x, y)
+            ##################################################
+            # 종료할경우 gRunning를 죽인다.
+            if gWhatScenes == False:
+                game_framework.quit()
+            ##################################################
+            pass
+        """
+        if event.type == SDL_KEYDOWN and event.key == SDLK_KP_5:
+            gType += 1
+            if gType == 12:
+                gType = 0
+        if event.type == SDL_KEYDOWN and event.key == SDLK_UP:
+            gFrame = (gFrame + 1) % 3
+        if event.type == SDL_KEYDOWN and event.key == SDLK_LEFT:
+            gLeftTRightF = True
+        if event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
+            gLeftTRightF = False
+        """
+    pass
+
+def enter():
+    global lBackGround, lPlanet, lMenu, lTitle, lBack
+    open_canvas(gCanvasWidth, gCanvasHeight)
+    lBackGround = BackGround()
+    # cBackGround라는 클래스를 BackGround로 가져오기
+    lPlanet = DrawPlanet()
+    # 클래스 함수를 만들어서 행성이 보이게 만들기
+    lMenu = DrawMenu()
+    # 클래스 함수를 만들어서 메뉴 만들기
+    lTitle = DrawTitle()
+    # 클래스 함수를 만들어서 타이틀 만들기
+    lBack = DrawBack()
+    # 클래스 함수를 만들어서 뒤로가기 만들기
+    pass
+
+
+def update():
+    global gY, gY2
+    gY, gY2 = dAutoSlideBG(gY, gY2)                                                    # 이미지 내려주는 함수
+    pass
+
+def draw():
+    global lBackGround, lPlanet, lMenu, lTitle, lBack
+    global gCanvasWidth, gCanvasHeight
+    clear_canvas()
+    lBackGround.draw(gY, gY2, gCanvasWidth, gCanvasHeight, 0)                         # 배경 그려주는 함수
+    lBackGround.draw(gY, gY2, gCanvasWidth, gCanvasHeight, 1)                         # 배경 그려주는 함수
+    lPlanet.dDraw()                                                                   # 행성 그려주는 함수
+
+
+    lTitle.dDraw(240,550)                   # Title
+    lMenu.dDraw(3,240,400)                  # Easy
+    lMenu.dDraw(4,240,250)                  # Middle
+    lMenu.dDraw(5,240,100)                  # Hards
+    lBack.dDraw(22,22)                      # Back
+
+    update_canvas()
+    delay(0.015)
+    pass
+
+def exit():
+    global lBackGround, lPlanet, lMenu, lTitle, lBack
+    del(lBackGround)
+    del(lPlanet)
+    del(lMenu)
+    del(lTitle)
+    del(lBack)
+    print("Main.py ByeBye~!!!")
+    pass
