c0cbab330288f1058a08b98613312518d53ffbff
WindowsHyun <kwon616@gmail.com>
2015-10-29 18:46:35 +0900

    Update game_ing Code
    캐릭터 로켓 애니메이션 추가
    easy, middle, hard 에 따른 점프 최대치 받아오는 함수 추가
    발판을 제작하기 위한 22*30 gGameMap 배열 추가
    class_data에서 현재 진행중인 게임이 무슨 난이도인지 확인하기 위한 기능 추가
    스페이스 키로 하여금 로켓 기능 테스트중..!
    발판이 생성이후 어느정도 지나면 내려오게 하는 기능(7주차)
    발판에 대한 충돌체크 확인하는 부분 (7주차)
    특정 발판을 밟을시 해당 발판이 변경되게 하는 부분

--- game_ing.py
+++ game_ing.py
@@ -4,6 +4,9 @@ from class_data import *
 #print("- import Class Data -")
 import game_framework
 #print("- Module game_framework -")
+#print("- Module random -")
+import game_select
+#print("- Module main -")
 
 gCanvasWidth = 480
 gCanvasHeight = 800
@@ -18,20 +21,38 @@ gRabbitY = 100
 gRabbitX = 100
 gRabbitYD = 0
 gRabbitR = 0
+gRabbitJet = False
+gRabbitJetStart = False
+gJetFrame = 0
+gRabbitLimitJump = 10
 
-gtest = 0
+gCol = 22
+gRow = 30
+gGameMap = [[0 for col in range(gCol)] for row in range(gRow)]
+for i in range(gRow): #gCol==> gRow
+    for j in range(gCol):  #gRow==> gCol
+        gGameMap[i][j] = -1
+gGameMapCheck = [0 for row in range(gRow)]
+gRandLine = 2
+
+nowRMenu = None
 print("gmae-ing.py : Create Local -> Global function")
 
 def handle_events():
     global gRunning, gBackGround, gFrame, gLeftTRightF, gWhatScenes, gType
+    global gGameMap, gGameMapCheck, gCol, gRow, gRandLine, gRabbitY, gRabbitX, gRabbitYD, gRabbitR, gRabbitJet
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT or event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             # 종료버튼을 누르거나 or 키보드의 ESC 키를 누를경우 종료를 한다.
-            game_framework.quit()
+            dUpdateMenu("Game_Select")
+            gRabbitY ,gRabbitX, gRabbitYD, gRabbitR = 100, 100, 0, 0
+            gGameMap, gGameMapCheck, gRandLine = dResetMap(gCol, gRow, gGameMap, gGameMapCheck)
+            game_framework.change_state(game_select)
         if event.type == SDL_MOUSEBUTTONDOWN:
             x, y = event.x, gCanvasHeight - event.y
-            gWhatScenes = dMenuClick(gWhatScenes, x, y)
+            #gWhatScenes = dMenuClick(gWhatScenes, x, y)
+            print(x, "-", y)
             ##################################################
             # 종료할경우 gRunning를 죽인다.
             if gWhatScenes == False:
@@ -41,52 +62,115 @@ def handle_events():
             gLeftTRightF = True
         if event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
             gLeftTRightF = False
+        if event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            gRabbitJet = True
     pass
 
 def enter():
-    global lBackGround, lMiscPictures, gWhatScenes, lRabbit, lFootrest
-    gWhatScenes = "Easy"
+    global lBackGround, lMiscPictures, gWhatScenes, lRabbit, lFootrest, lRabbitJet
+    gWhatScenes = dShowMenu()
     print("Open : game_ing.py Code")
     lBackGround = BackGround()
     # cBackGround라는 클래스를 BackGround로 가져오기
     lMiscPictures = DrawMiscPictures()
     # 클래스 함수를 만들어서 여러가지 이미지 불러오기
     lRabbit = cDrawRabbit()
+    lRabbitJet = cDrawRabbitJet()
     lFootrest = DrawFootrest()
+    gGameMap[1][3] = 0
+    gGameMap[1][8] = 0
+    gGameMap[1][13] = 0
+    gGameMap[1][18] = 0
     pass
 
 
 def update():
-    global lRabbit, gFrame, gRabbitYD, gRabbitY, gRabbitR, gtest, gRabbitX, gLeftTRightF, gCanvasWidth
-
+    global lRabbit, lRabbitJet, gFrame, gRabbitYD, gRabbitY, gRabbitR, gRabbitX, gLeftTRightF, gCanvasWidth, gRabbitLimitJump, gRabbitJet
+    global gGameMapCheck, gGameMap, gRow, gRandLine, gWhatScenes
     gFrame, gRabbitY, gRabbitR = lRabbit.dUpdateRabbitUpDown(gFrame, gRabbitYD, gRabbitY, gRabbitR)
-    gFrame, gRabbitR, gRabbitYD, gtest = lRabbit.dLimitJump(gFrame, gRabbitR, gRabbitYD, gtest)
+    gFrame, gRabbitR, gRabbitYD = lRabbit.dLimitJump(gFrame, gRabbitR, gRabbitYD, gRabbitLimitJump)
     gRabbitX = lRabbit.dRabbitMove(gLeftTRightF, gRabbitX)
     gRabbitX, gLeftTRightF = lRabbit.dRabbitPass(gRabbitX, gLeftTRightF, gCanvasWidth)
-    delay(0.05)
+    gRandLine, gGameMapCheck, gGameMap, gRabbitLimitJump = dCreateFootrest(gRow, gRandLine, gGameMapCheck, gGameMap, gWhatScenes, gRabbitLimitJump)
+    gRabbitYD, gRabbitR, gRabbitJet, gGameMap = dFootrestCheck(gCol, gRow, gRabbitX, gRabbitY, gRabbitYD, gRabbitR, gGameMap, gRabbitJet)
+    dMapDown()
+    dHideFootRest()
+
+
+    delay(0.015)
     pass
 
 def draw():
-    global lBackGround, lMiscPictures, lRabbit, lFootrest
-    global gCanvasWidth, gCanvasHeight, gLeftTRightF, gFrame, gRabbitX, gRabbitY, gtest
+    global lBackGround, lMiscPictures, lRabbit, lFootrest, gRabbitJet, gJetFrame
+    global gCanvasWidth, gCanvasHeight, gLeftTRightF, gFrame, gRabbitX, gRabbitY, gtest, gRabbitYD
+    global gCol, gRow, gGameMap
     clear_canvas()
 
     lBackGround.draw(gY, gY2, gCanvasWidth, gCanvasHeight, 0)                         # 배경 그려주는 함수
     lBackGround.draw(gY, gY2, gCanvasWidth, gCanvasHeight, 1)                         # 배경 그려주는 함수
     lMiscPictures.dDraw("planet", 415, 723)
 
-    lRabbit.dDraw(gFrame, gLeftTRightF, gRabbitX, gRabbitY)
+    if ( gRabbitJet == False ):
+        lRabbit.dDraw(gFrame, gLeftTRightF, gRabbitX, gRabbitY)
+    else:
+        lRabbitJet.dDraw(2, 0, gRabbitX, gRabbitY)
+        dRabbitJet()
 
-    lFootrest.dDraw(gtest,55,50)
+    for i in range(gCol):
+        for j in range(gRow):
+            lFootrest.dDraw(gGameMap[j][i],(gCol) * i, gRow * j)
 
-    lFootrest.dDraw(gtest,182,50)
-    lFootrest.dDraw(gtest,298,50)
-
-    lFootrest.dDraw(gtest,425,50)
+    dFontDraw(3,10, gWhatScenes, 255, 255, 255)
 
     update_canvas()
     delay(0.015)
     pass
+
+def dMapDown():
+    global gY, gY2, gRabbitY, gCanvasHeight, gRabbitYD
+    global gGameMap, gGameMapCheck, gRandLine
+    if( gRabbitY >= gCanvasHeight-300):
+        for i in range(10):
+            gY, gY2 = dAutoSlideBG(gY, gY2)
+        gRabbitYD = 2
+        gGameMap, gGameMapCheck, gRandLine = dMapAllDown(gGameMap, gGameMapCheck, gCol, gRow, gRandLine)
+    if gRabbitR >= gRabbitLimitJump:
+        gRabbitYD = 1
+        pass
+    pass
+
+def dHideFootRest():
+    for i in range(gCol):
+        for j in range(gRow):
+            if ( gGameMap[j][i] == 8 ):
+                gGameMap[j][i] = -1
+            if ( gGameMap[j][i] == 7 ):
+                gGameMap[j][i] = 8
+            if ( gGameMap[j][i] == 6 ):
+                gGameMap[j][i] = 7
+    pass
+
+def dRabbitJet():
+    global gRabbitJet, gRabbitJetStart, gJetFrame, gRabbitY, gRabbitYD, gFrame, gRabbitR
+    if ( gRabbitJet == True and gRabbitJetStart == False ):
+        gRabbitJetStart = True
+    elif ( gRabbitJet == True and gRabbitJetStart == True ):
+        gJetFrame += 1
+        if( gRabbitY <= gCanvasHeight-300):
+            gRabbitY += 12
+        else:
+            gRabbitY = gCanvasHeight-300
+        gRabbitYD = 0
+        if ( gJetFrame >= 100 ):
+            gRabbitYD = 1
+            gJetFrame = 0
+            gRabbitR = 0
+            gFrame = 1
+            gRabbitJetStart = False
+            gRabbitJet = False
+        pass
+    pass
+
 
 def exit():
     global lBackGround, lMiscPictures, lRabbit, lFootrest
