7af128d1a0e819862d5be0bdd56889bb35c957a8
WindowsHyun <kwon616@gmail.com>
2015-11-01 19:22:50 +0900

    Upload game_help Code
    게임의 도움말 등을 설명 하기 위한 Help메뉴 제작

---
+++ game_help.py
@@ -0,0 +1,81 @@
+__author__ = 'Administrator'
+
+from class_data import *
+#print("- import Class Data -")
+import game_framework
+#print("- Module game_framework -")
+import main
+#print("- Module main -")
+
+gCanvasWidth = 480
+gCanvasHeight = 800
+gY = 0
+gY2 = 800
+gFrame = 0
+gType = 0
+gRunning = True
+gLeftTRightF = True
+gBackGround = 0
+nowRMenu = None
+print("gmae_help.py : Create Local -> Global function")
+
+def handle_events():
+    global gRunning, gBackGround, gFrame, gLeftTRightF, gWhatScenes, gType
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT or event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            # 종료버튼을 누르거나 or 키보드의 ESC 키를 누를경우 종료를 한다.
+            game_framework.quit()
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            x, y = event.x, gCanvasHeight - event.y
+            gWhatScenes = dMenuClick(gWhatScenes, x, y)
+            ##################################################
+            # 종료할경우 gRunning를 죽인다.
+            if gWhatScenes == False:
+                game_framework.quit()
+            if gWhatScenes == "Game_Main":
+                dUpdateMenu(gWhatScenes)
+                game_framework.change_state(main)
+            ##################################################
+            pass
+    pass
+
+def enter():
+    global lBackGround, lMiscPictures, gWhatScenes
+    gWhatScenes = "Game_Help"
+    print("Open : game_help.py Code")
+    lBackGround = BackGround()
+    # cBackGround라는 클래스를 BackGround로 가져오기
+    lMiscPictures = DrawMiscPictures()
+    # 클래스 함수를 만들어서 여러가지 이미지 불러오기
+    dUpdateMenu(gWhatScenes)
+    pass
+
+
+def update():
+    global gY, gY2
+    gY, gY2 = dAutoSlideBG(gY, gY2)                                                    # 이미지 내려주는 함수
+    pass
+
+def draw():
+    global lBackGround, lMiscPictures
+    global gCanvasWidth, gCanvasHeight
+    clear_canvas()
+
+    lBackGround.draw(gY, gY2, gCanvasWidth, gCanvasHeight, 0)                         # 배경 그려주는 함수
+    lBackGround.draw(gY, gY2, gCanvasWidth, gCanvasHeight, 1)                         # 배경 그려주는 함수
+    lMiscPictures.dDraw("planet", 415, 723)
+    lMiscPictures.dDraw("back", 22, 22)
+
+    dFontDraw(3,10, gWhatScenes, 255, 255, 255)
+
+    update_canvas()
+    delay(0.015)
+    pass
+
+def exit():
+    global lBackGround, lMiscPictures
+    del(lBackGround)
+    del(lMiscPictures)
+    print("Unload : game_help.py Code")
+    pass
