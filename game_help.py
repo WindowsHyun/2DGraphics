66af4f00cbf91c38ab8a361b9e1fad6185d90719
WindowsHyun <kwon616@gmail.com>
2015-11-01 20:40:09 +0900

    Update game_help Code
    토끼 불러오는 변수 추가
    발판 설명을 위한 발판 배열 추가
    마우스 클릭시 X,Y 좌표 기능 추가 (발판 클릭시 해당 발판으로 바뀌기 위하여
    키보드 좌,우 기능 추가 (토끼가 좌,우 이미지 바뀌는것을 보여주기 위하여)
    그 외 발판맵 설정 및 class_data에서 토끼의 점프, 발판 충돌체크 등의 함수를 불러옴!
    
    문제 : 한글 폰트 지원이 되지 않아서 현재 작성이 불가능... 이미지 파일로 일일히 불러오기에는 너무 낭비인 느낌이 든다.
    해결방법을 찾아보자..!

--- game_help.py
+++ game_help.py
@@ -14,9 +14,30 @@ gY2 = 800
 gFrame = 0
 gType = 0
 gRunning = True
-gLeftTRightF = True
 gBackGround = 0
 nowRMenu = None
+######################################################
+# 토끼 불러오는 함수
+gLeftTRightF = True
+gRabbitY = 237
+gRabbitX = 417
+gRabbitYD = 0
+gRabbitR = 0
+gRabbitJet = False
+gRabbitJetStart = False
+gJetFrame = 0
+gRabbitLimitJump = 10
+######################################################
+gCol = 22
+gRow = 30
+gGameMap = [[0 for col in range(gCol)] for row in range(gRow)]
+for i in range(gRow): #gCol==> gRow
+    for j in range(gCol):  #gRow==> gCol
+        gGameMap[i][j] = -1
+gGameMapCheck = [0 for row in range(gRow)]
+gRandLine = 2
+######################################################
+
 print("gmae_help.py : Create Local -> Global function")
 
 def handle_events():
@@ -28,6 +49,7 @@ def handle_events():
             game_framework.quit()
         if event.type == SDL_MOUSEBUTTONDOWN:
             x, y = event.x, gCanvasHeight - event.y
+            print(x, "-", y)
             gWhatScenes = dMenuClick(gWhatScenes, x, y)
             ##################################################
             # 종료할경우 gRunning를 죽인다.
@@ -37,35 +59,68 @@ def handle_events():
                 dUpdateMenu(gWhatScenes)
                 game_framework.change_state(main)
             ##################################################
-            pass
+        if event.type == SDL_KEYDOWN and event.key == SDLK_LEFT:
+            gLeftTRightF = True
+        if event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
+            gLeftTRightF = False
     pass
 
 def enter():
-    global lBackGround, lMiscPictures, gWhatScenes
+    global lBackGround, lMiscPictures, gWhatScenes, lRabbit, lFootrest, lRabbitJet, gGameMap
     gWhatScenes = "Game_Help"
     print("Open : game_help.py Code")
     lBackGround = BackGround()
     # cBackGround라는 클래스를 BackGround로 가져오기
     lMiscPictures = DrawMiscPictures()
     # 클래스 함수를 만들어서 여러가지 이미지 불러오기
+    lRabbit = cDrawRabbit()
+    lRabbitJet = cDrawRabbitJet()
+    lFootrest = DrawFootrest()
+
+    gGameMap[24][3] = 0
+    gGameMap[20][3] = 1
+    gGameMap[16][3] = 4
+    gGameMap[12][3] = 5
+    gGameMap[8][3] = 12
+    gGameMap[4][3] = 9
+
+    #토끼, 발판 이미지 불러오기
     dUpdateMenu(gWhatScenes)
     pass
 
 
 def update():
-    global gY, gY2
+    global gY, gY2, lRabbit, lRabbitJet
+    global gRabbitX, gLeftTRightF, gRabbitYD, gRabbitR, gRabbitJet, gGameMap, gCol, gRow, gFrame, gRabbitY, gRabbitLimitJump
     gY, gY2 = dAutoSlideBG(gY, gY2)                                                    # 이미지 내려주는 함수
+    gFrame, gRabbitY, gRabbitR = lRabbit.dUpdateRabbitUpDown(gFrame, gRabbitYD, gRabbitY, gRabbitR)
+    gFrame, gRabbitR, gRabbitYD = lRabbit.dLimitJump(gFrame, gRabbitR, gRabbitYD, gRabbitLimitJump)
+    gRabbitX, gLeftTRightF = lRabbit.dRabbitPass(gRabbitX, gLeftTRightF, gCanvasWidth)
+    gRabbitYD, gRabbitR, gRabbitJet, gGameMap = dFootrestCheck(gCol, gRow, gRabbitX, gRabbitY, gRabbitYD, gRabbitR, gGameMap, gRabbitJet)
+    delay(0.015)
     pass
 
 def draw():
     global lBackGround, lMiscPictures
-    global gCanvasWidth, gCanvasHeight
+    global gCanvasWidth, gCanvasHeight, gLeftTRightF, gRabbitX, gRabbitY, gFrame
     clear_canvas()
 
     lBackGround.draw(gY, gY2, gCanvasWidth, gCanvasHeight, 0)                         # 배경 그려주는 함수
     lBackGround.draw(gY, gY2, gCanvasWidth, gCanvasHeight, 1)                         # 배경 그려주는 함수
     lMiscPictures.dDraw("planet", 415, 723)
     lMiscPictures.dDraw("back", 22, 22)
+
+    if ( gRabbitJet == False ):
+        lRabbit.dDraw(gFrame, gLeftTRightF, gRabbitX, gRabbitY)
+    else:
+        lRabbitJet.dDraw(2, 0, gRabbitX, gRabbitY)
+        #dRabbitJet()
+
+    gGameMap[4][19] = 0
+
+    for i in range(gCol):
+        for j in range(gRow):
+            lFootrest.dDraw(gGameMap[j][i],(gCol) * i, gRow * j)
 
     dFontDraw(3,10, gWhatScenes, 255, 255, 255)
 
