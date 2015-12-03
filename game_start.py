e5aef0f5bd43572332d1f18f93df8b1f973aaf05
WindowsHyun <kwon616@gmail.com>
2015-12-03 16:59:55 +0900

    Upload Setup Data [ exe 만든후 설치할수있게 인스톨러를 만듬 ]
    Setup 폴더에 exe 파일및 설치파일 제작및 리소스 추가
    game_start.py 에서 SDL이 없을경우에 대한 SDL 실행문구 추가.

--- game_start.py
+++ game_start.py
@@ -1,5 +1,12 @@
 __author__ = 'Administrator'
 
+import platform
+import os
+if platform.architecture()[0] == '32bit':
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86"
+else:
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
+
 import game_framework
 import game_title
 
